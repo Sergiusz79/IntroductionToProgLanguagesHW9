d06b7afd357ff87805837f6b8e30a9ff5b999042
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-05-31 22:52:35 +0200

    Task064

--- Task064/Program.cs
+++ Task064/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+//M = 1; N = 5. -> "1, 2, 3, 4, 5"
+//M = 4; N = 8. -> "4, 6, 7, 8"
+
+
+int EnterNumber(string text)
+{
+    int number;
+    Console.WriteLine(text);
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("This is not number. Please enter number.");
+    }
+    return number;
+}
+
+
+
+string WriteIntegersP(int m, int n)
+{
+    if (m <= n) return $"{m} " + WriteIntegersP(m + 1, n);
+    else return String.Empty;
+}
+string WriteIntegersN(int m, int n)
+{
+   if (m >= n) return WriteIntegersN(m, n + 1) + $"{n} ";
+    else return String.Empty;
+}
+
+void Task064()
+{
+    int m = EnterNumber("Please enter first number:> ");
+    int n = EnterNumber("Please enter second number:> ");
+    
+    if (m <= n) System.Console.WriteLine(WriteIntegersP(m, n));
+    if (m >= n) System.Console.WriteLine(WriteIntegersN(m, n));
+}
+
+Task064();
--- Task066/Program.cs
+++ Task066/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+
+int EnterNumber(string text)
+{
+    int number;
+    Console.WriteLine(text);
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("This is not number. Please enter number.");
+    }
+    return number;
+}
+
+
+
+// string WriteIntegersN(int m, int n)
+// {
+//     if (m < n) return $"{m} " + WriteIntegers(m + 1, n);
+
+//     else return $"{m}";
+
+//     //if (m > n) return n + WriteIntegers(m, n + 1);
+// } 
+
+int WriteIntegers(int m, int n)
+{
+    if (m < n) return m + WriteIntegers(m + 1, n);
+    if (m > n) return n + WriteIntegers(m, n + 1);
+    else return m;
+
+    //if (m > n) return n + WriteIntegers(m, n + 1);
+} 
+
+
+
+void Task066()
+{
+    int m = EnterNumber("Please enter first number:> ");
+    int n = EnterNumber("Please enter second number:> ");
+
+    System.Console.WriteLine(WriteIntegers(m, n));
+}
+
+Task066();
